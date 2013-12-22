142e720425222ec700ed20c75f2ccb1eb093a504
TWILIGHT-IS-AWESOME <pipino666@ymail.com>
2013-12-22 13:21:05 -0200

    Update fc_userscript_loader.js

--- fc_userscript_loader.js
+++ fc_userscript_loader.js
@@ -1,22 +1,16 @@
 // ==UserScript==
 // @name           Frozen Cookies
 // @version        github-latest
-// @description    Userscript to load Frozen Cookies written by Icehawk78
+// @description    Userscript to load Frozen Cookies written by Icehawk78, modified by TWILIGHT_IS_AWESOME
 // @author         shinji257
-// @homepage       https://github.com/Icehawk78/FrozenCookies
+// @homepage       https://github.com/TWILIGHT-IS-AWESOME/FrozenCookies
 // @include        http://orteil.dashnet.org/cookieclicker/
-// @updateURL      http://icehawk78.github.io/FrozenCookies/fc_userscript_loader.js
-// @downloadURL    http://icehawk78.github.io/FrozenCookies/fc_userscript_loader.js
 // ==/UserScript==
-
-// Dev:       https://raw.github.com/Icehawk78/FrozenCookies/development/
-// Master:    https://raw.github.com/Icehawk78/FrozenCookies/master/
-// Github.io: http://icehawk78.github.com/FrozenCookies/
 
 function LoadFrozenCookies() {
   var js = document.createElement('script');
   js.setAttribute('type', 'text/javascript');
-  js.setAttribute('src', 'https://raw.github.com/Icehawk78/FrozenCookies/master/frozen_cookies.js');
+  js.setAttribute('src', 'https://raw.github.com/TWILIGHT-IS-AWESOME/FrozenCookies/master/frozen_cookies.js');
   document.head.appendChild(js);
 }
 // It's not the best way but Chrome doesn't work with addEventListener... :(
